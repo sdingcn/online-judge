cb4b89a381d0cb3fefad1d9794d9eff34148ecc5
Shuo Ding <sdingcn@outlook.com>
2023-04-13 15:59:58 -0400

    better UI

--- checker.py
+++ checker.py
@@ -13,19 +13,19 @@ def check(code):
             f.write(code)
         output = run_and_get_output(['clang++', f'{name}.cpp', '-o', f'{name}.executable']).strip()
         if output != '':
-            return 'Compilation error: ' + output[:100] + '...'
+            return ('CE', output[:100])
         def check_io(stdin, stdout):
             if run_and_get_output([f'./{name}.executable'], stdin).strip() == stdout.strip():
                 return True
             else:
                 return False
         if not check_io('0', '0'):
-            return 'Wrong answer on test 1'
+            return ('WA', 'Wrong answer on test 1')
         if not check_io('3 2 1', '1 2 3'):
-            return 'Wrong answer on test 2'
+            return ('WA', 'Wrong answer on test 2')
         if not check_io('1 2 3 4 5', '1 2 3 4 5'):
-            return 'Wrong answer on test 3'
-        return 'Accepted'
+            return ('WA', 'Wrong answer on test 3')
+        return ('AC', 'Accepted')
     finally:
         if os.path.exists(f'{name}.cpp'):
             os.remove(f'{name}.cpp')
--- server.py
+++ server.py
@@ -2,16 +2,21 @@ from wsgiref.simple_server import make_server, WSGIServer
 from urllib.parse import parse_qs
 from socketserver import ThreadingMixIn
 import checker
+import datetime
 
 def application(environ, start_response):
     path = environ['PATH_INFO']
     method = environ['REQUEST_METHOD']
     if path == '/' and method == 'POST':
-        code = parse_qs(
+        data = parse_qs(
             environ['wsgi.input'].read(int(environ['CONTENT_LENGTH'])).decode(),
-            keep_blank_values = True)['code'][0]
+            keep_blank_values = True)
+        name = data['name'][0]
+        code = data['code'][0]
+        result = checker.check(code)
+        print(f'Date and Time: {str(datetime.datetime.now())}, Name: {name}, Verdict: {result[0]}')
         with open('pages/result.html', 'r') as f:
-            response = f.read().format(checker.check(code)).encode()
+            response = f.read().format(name, *result).encode()
         status = '200 OK'
     elif path == '/' and method == 'GET':
         with open('pages/submission.html', 'r') as f:
