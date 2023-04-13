711c4fc0171a06a0d2550e72dd76b6e75ccedcb6
Shuo Ding <sdingcn@outlook.com>
2023-04-13 17:45:00 -0400

    log

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # online-judge
 
+## Introduction
+
+## Prepare your problem
+
+## Start/stop the server
 Run `python3 server.py` to start the web server on `localhost:8008`.
--- server.py
+++ server.py
@@ -3,6 +3,19 @@ from urllib.parse import parse_qs
 from socketserver import ThreadingMixIn
 import problem
 import datetime
+import threading
+import time
+import re
+
+L = threading.Lock()
+
+def log(timestamp: str, name: str, code: str, score: str, details: str) -> None:
+    underscored_name = re.sub(r"\s+", "_", name)
+    prefix = f'logs/{timestamp}-{underscored_name}-{score}'
+    with open(prefix + '.code', 'w') as f:
+        f.write(code)
+    with open(prefix + '.details', 'w') as f:
+        f.write(details)
 
 def application(environ, start_response):
     path = environ['PATH_INFO']
@@ -11,12 +24,14 @@ def application(environ, start_response):
         data = parse_qs(
             environ['wsgi.input'].read(int(environ['CONTENT_LENGTH'])).decode(),
             keep_blank_values = True)
+        timestamp = str(time.time())
         name = data['name'][0]
         code = data['code'][0]
-        result = problem.judge(code)
-        print(f'Date and Time: {str(datetime.datetime.now())}, Name: {name}, Score: {result[0]}')
+        score, details = problem.judge(code)
+        with L:
+            log(timestamp, name, code, score, details)
         with open('pages/result.html', 'r') as f:
-            response = f.read().format(name, *result).encode()
+            response = f.read().format(name, score, details).encode()
         status = '200 OK'
     elif path == '/' and method == 'GET':
         with open('pages/submission.html', 'r') as f:
@@ -36,4 +51,7 @@ class ThreadingWSGIServer(ThreadingMixIn, WSGIServer):
     pass
 
 if __name__ == '__main__':
-    make_server('localhost', 8008, application, ThreadingWSGIServer).serve_forever()
+    try:
+        make_server('localhost', 8008, application, ThreadingWSGIServer).serve_forever()
+    except KeyboardInterrupt:
+        print('Server stopped.')
