652c378654cc63287b970138e68325a3c72db487
Shuo Ding <sdingcn@outlook.com>
2023-04-13 21:42:40 -0400

    ctrl c

--- server.py
+++ server.py
@@ -65,3 +65,4 @@ if __name__ == '__main__':
         make_server(host, port, application, ThreadingWSGIServer).serve_forever()
     except KeyboardInterrupt:
         print('Server stopped.')
+        sys.exit(0)
